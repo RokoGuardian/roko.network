346abd1e86618b983fb9e7078c44b6dcd16fbddc
robit <36677806+robit-man@users.noreply.github.com>
2023-01-21 17:17:14 +0000

    mezz

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -4,6 +4,14 @@
     
             <div class="middle">
                 <h1>MEZZ</h1>
+                <div class="row">
+                    <a href="https://twitter.com/MEZZTOKEN">
+                        <p>TWITTER</p>
+                    </a>
+                    <a href="https://dexscreener.com/ethereum/0x1ad651a07b98464aa42f6cad68d0ce8639b659a5">
+                        <p>TWITTER</p>
+                    </a>
+                </div>
             </div>
     
         </div>
@@ -26,20 +34,33 @@ p {
 
 .middle {
     margin: auto;
+    transform: rotate(-20deg);
 }
 
 .middle>h1 {
     font-size: 10vh;
-    transform: rotate(-20deg);
     color: white;
     text-shadow: 0px 0px 100px white;
 }
 
-@media screen and (min-width: 100vh
-) {
+.row {
+    display: flex;
+    flex-flow: row;
+    justify-content: space-around;
+}
+
+a {
+    color: white;
+    text-decoration: unset;
+}
+
+a>p {
+    text-shadow: 0px 0px 10px white;
+}
+
+@media screen and (min-width: 100vh) {
     .middle>h1 {
         font-size: 10vw;
-        transform: rotate(-20deg);
         color: white;
         text-shadow: 0px 0px 100px white;
     }
